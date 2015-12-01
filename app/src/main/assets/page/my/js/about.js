4cb57bbc4c0ad35bba0e5d712587babc8ec201a1
zhuotianxiao <zhuotianxiao@126.com>
2015-12-01 09:54:52 +0800

    version 4 dbversion 5 提交前修改

--- app/src/main/assets/page/my/js/about.js
+++ app/src/main/assets/page/my/js/about.js
@@ -5,3 +5,22 @@ function update() {
 
 	SDK.update();
 }
+
+
+function openDail(obj) {
+
+	var tel = $(obj).text();//alert(tel);
+	SDK.dail(tel);
+}
+
+function openMail(obj) {
+
+	var email = $(this).text();
+	SDK.mailTo(email);
+}
+
+function openWebSite(obj) {
+
+	var url = $(this).text();
+	SDK.openPage("http://www.fundview.cn");
+}
